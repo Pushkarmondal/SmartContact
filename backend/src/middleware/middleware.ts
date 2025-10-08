94325dc79f2c0e397e086e3f3efb8ae8334a9e85
Pushkar Mondal <nishitm060@gmail.com>
2025-10-08 23:16:28 +0530

    Auth routes ✅

--- backend/src/middleware/middleware.ts
+++ backend/src/middleware/middleware.ts
@@ -1,17 +1,21 @@
-import type { Request, Response, NextFunction } from "express"
-import jwt, { type JwtPayload } from "jsonwebtoken"
+import type { Request, Response, NextFunction } from "express";
+import jwt, { type JwtPayload } from "jsonwebtoken";
 
-export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const token = req.headers.authorization?.split(" ")[1];
-        if (!token) {
-            return res.status(401).json({ message: "Unauthorized" });
-        }
-        const decodedToken = jwt.verify(token, "secret") as JwtPayload;
-        req.user = decodedToken;
-        next();
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: "Internal Server Error" });
+export const authMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) {
+      return res.status(401).json({ message: "Unauthorized: No token" });
     }
-}
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET || "Your sectet here") as JwtPayload;
+    req.user = decodedToken;
+    next(); 
+  } catch (error) {
+    console.error("Auth Middleware Error:", error);
+    res.status(401).json({ error: "Invalid or expired token" });
+  }
+};
